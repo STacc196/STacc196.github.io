1be3ebe71445ebe6a182c4b39f7a20a5359b8937
Steven Lum <42789062+slum117@users.noreply.github.com>
2022-03-31 21:12:40 -0700

    finished work

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,4 +1,29 @@
+function helloWorld() {
+    alert("Hello, world!");
+}
+
 function bigger() {
     document.getElementById("text").style.fontSize = "24px";
 }
+
+function boringBetty() {
+    document.getElementById("text").style = "initial";
+}
+
+function fancySchmancy() {
+    document.getElementById("text").style.fontWeight = "bold";
+    document.getElementById("text").style.color = "blue";
+    document.getElementById("text").style.textDecoration = "underline";
+}
+
+function moo() {
+    var str =  document.getElementById("text").value;
+    str = str.toUpperCase()
+    var parts = str.split(".");
+    str = parts.join(". -Moo");
+    //let partsLength = parts.length;
+    if (parts.length <= 1) {str = str+"-Moo"}
+    document.getElementById("text").value = str;
+}
+
 
